597c1fec35a1e827ba01136e56fa0c53c36d2ffb
Ugur CANGAL <ugurcangal@gmail.com>
2022-11-04 15:16:42 +0300

    product page favorite button and back button completed!

--- app/src/main/java/com/ugurcangal/lufian/view/user/UserProductFragment.kt
+++ app/src/main/java/com/ugurcangal/lufian/view/user/UserProductFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ugurcangal.lufian.BaseFragment
@@ -17,6 +19,8 @@ import com.ugurcangal.lufian.viewmodel.user.UserProductViewModel
 class UserProductFragment : BaseFragment<FragmentUserProductBinding,UserProductViewModel>(FragmentUserProductBinding::inflate) {
 
     private lateinit var productId : String
+    private val firestore = Firebase.firestore
+    private val auth = Firebase.auth
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,6 +31,20 @@ class UserProductFragment : BaseFragment<FragmentUserProductBinding,UserProductV
         }
 
         viewModel.getProduct(binding, productId, view)
+        viewModel.favoriteControl(binding, productId)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.favoriteButton.setOnClickListener {
+            viewModel.addFavorite(productId, it)
+        }
+
+        binding.favoriteDeleteButton.setOnClickListener {
+            viewModel.deleteFavorite(binding, productId, it)
+        }
+
 
     }
 
--- app/src/main/java/com/ugurcangal/lufian/viewmodel/user/UserProductViewModel.kt
+++ app/src/main/java/com/ugurcangal/lufian/viewmodel/user/UserProductViewModel.kt
@@ -10,13 +10,46 @@ import javax.inject.Inject
 @HiltViewModel
 class UserProductViewModel @Inject constructor() : BaseViewModel(){
 
+    private var favorites = ArrayList<Any>()
+    private val favoritesMap = HashMap<String,Any>()
+
     fun getProduct(binding: FragmentUserProductBinding,productId: String,view : View){
         firestore.collection("Product").document(productId).get().addOnSuccessListener {
             binding.imageProgress.visibility = View.GONE
-            binding.productName.text = it.get("name").toString()
+            binding.productName.text = it.get("name").toString().uppercase()
             binding.productPrice.text = it.get("price").toString() + " TL"
             Glide.with(view).load(it.get("downloadUrl").toString()).into(binding.productImage)
         }
+    }
+
+    fun favoriteControl(binding: FragmentUserProductBinding, productId: String){
+        firestore.collection("Favorites").document(auth.currentUser!!.email.toString()).addSnapshotListener { value, error ->
+            value?.let {
+                favorites = it.get("favorites") as ArrayList<Any>
+                if (favorites.contains(productId)){
+                    binding.favoriteButton.visibility = View.GONE
+                    binding.favoriteDeleteButton.visibility = View.VISIBLE
+                }else{
+
+                }
+
+            }
+        }
+    }
+
+    fun addFavorite(productId: String, view: View){
+        favorites.add(productId)
+        favoritesMap.put("favorites",favorites)
+        firestore.collection("Favorites").document(auth.currentUser!!.email.toString()).set(favoritesMap)
+        view.visibility = View.GONE
+    }
+
+    fun deleteFavorite(binding: FragmentUserProductBinding,productId: String, view: View){
+        favorites.remove(productId)
+        favoritesMap.put("favorites",favorites)
+        firestore.collection("Favorites").document(auth.currentUser!!.email.toString()).update(favoritesMap)
+        view.visibility = View.GONE
+        binding.favoriteButton.visibility = View.VISIBLE
     }
 
 }
